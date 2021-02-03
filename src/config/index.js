5530bc7f3aee1264e8316d92eda99ef38824c5f9
Hassan PIOU <47037718+HassanPIOU@users.noreply.github.com>
2021-02-03 10:24:58 +0000

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,2 +1,6 @@
-export   const GOOGLE_CLIENTID = "30628054754-98n9im2id6tuk6mdvgteloc5n343md86.apps.googleusercontent.com"
-export const BLOG_TOKEN = "3f49f89f070f345a56a9ae55c9741fa2"
+// Google auth ClientId
+
+export   const GOOGLE_CLIENTID = ""
+
+// gnews.io token
+export const BLOG_TOKEN = ""
